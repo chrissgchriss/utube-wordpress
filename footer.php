6882cca569f6435d6b569ace7393a9bb4f28b1c4
chrissgchriss <chrissgchriss@gmail.com>
2015-06-08 16:40:46 -0700

    good time to commit

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
         <p>This is my footer</p>
         <?php wp_nav_menu(array('theme_location'=>'secondary'));?>
     </footer>
+    
+    </div><!-- this closes the menu container from header -->
     <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -1,15 +1,66 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Awesome Theme</title>
-		<?php wp_head(); ?>
-	</head>
-	
-	<body>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>Awesome Theme</title>
+            <?php wp_head(); ?>
+        </head>
+
+            <!-- if the index page load - then attach awesome-css, if not don't -->
+            <?php if (is_front_page() ):
+                $awesome_classes = array('awesome-class', 'my-class');
+                else:
+                $awesome_classes = array('no-awesome-class');
+                endif;
+            ?>
+        <body <?php body_class( $awesome_classes ); ?>>
+        
+            <div class="container"><!-- this class is closed in the footer -->
+                <div class="row">
+                    <div class="col-xs-12"> 
+                    <nav class="navbar navbar-default">
+                      <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                          <a class="navbar-brand" href="#">Awesome Theme</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <?php
+                        wp_nav_menu(array(
+                                                'theme_location'=>'primary',
+                                                'container'=>false,
+                                                'menu_class' => 'nav navbar-nav navbar-right'
+                                                )
+                                          );
+                        ?>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling 
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                          <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                          </ul>
+                        </div><!-- /.navbar-collapse -->
+                      </div><!-- /.container-fluid -->
+                    </nav>
+                        
+                    </div>
+                </div>
             
-            <?php wp_nav_menu(array('theme_location'=>'primary'));?>
-            <div style="width: 200px; height: 175px; border: thin solid red; overflow: hidden;">
+            
             <img src="<?php header_image(); ?>"
             height="<?php echo get_custom_header()->height; ?>"
-            width="<?php echo get_custom_header()->width; ?>" alt="" /></div>
+             width="<?php echo get_custom_header()->width; ?>"
+            alt=""
+            />
+        
+ 
+  
+  
+
+   
