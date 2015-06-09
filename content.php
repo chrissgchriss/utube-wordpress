926d50352e27123721a8b130ebe4e855d1e728fa
chrissgchriss <chrissgchriss@gmail.com>
2015-06-08 20:20:40 -0700

    up till part 7 - watched the hour and half long webinar

--- content.php
+++ content.php
@@ -1,7 +1,32 @@
-<h3> <?php the_title(); ?></h3>
-<small>Posted on:<?php the_time('Fj, Y'); ?> at <?php the_time('g:i a'); ?>, in
-<?php the_category(); ?></small>
-<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>        
-<p><?php the_content(); ?></p>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    
+    <header class="entry-header">
+        <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">',esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+        <small>Posted on:<?php the_time('Fj, Y'); ?> at <?php the_time('g:i a'); ?>, in
+        <?php the_category(); ?></small>
+    </header>
+   
+   <div class="row">
+    
+        <?php if( has_post_thumbnail() ): ?>
+            <div class="col-xs-12 col-sm-4">
+                <div class="thumbnail"><?php the_post_thumbnail('monkey'); ?></div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-8">
+                <?php the_content(); ?>
+            </div>  
+        <?php else: ?>
+            <div class="col-xs-12">
+                <?php the_content(); ?>
+            </div>
+            
+        <?php endif; ?>
+   </div>
+   
 <p>Shai Labeauf says just do it!!!</p>
 <hr />
+
+</article>
+
+
