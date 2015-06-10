e1b80e5c45ff1d8498169d5eb3d430b5b46dc4f2
chrissgchriss <chrissgchriss@gmail.com>
2015-06-09 17:46:28 -0700

    working version of this lesson

--- comments-file.php
+++ comments-file.php
@@ -1,28 +1,42 @@
 <?php get_header(); ?>
 
 <div class="row">
-
+    
+    
         <?php
+        
+        // 1. create an array of arguments for our category array filter
         $args_cat = array(
-            'include' => '1,9,8' 
+            'include' => '1,9,8' // 1,9,8 needs to be specified as a string, not an array value
         );
-        
+        //2. call these specific categories and loop throuhg these categories
+        // This variable will store inside all the categories we are looking for, like &statement in Obj C
         $categories = get_categories($args_cat);
-        // use var_dump($categories) to see what is the response
-        foreach($categories as $category): // checks how many objects are in args_cat 1,9,8
-        
-            $args = array( // goes through it first time, second time, third time
+        // var_dump($categories); this shows what we retried from our get_categories function
+        // now just to see what is inside our categories we can do a var_dump
+        // now we check...we see lots of info, but we only need 3 things the id of our categories
+        //OK
+        // now we need to loop throuhg what was inside of $categories
+        // foreach function
+        foreach($categories as $category);
+        // now inside this loop...we call custom php query -- copy our customized query below...
+        // now we wanted to only have 1 blog post per page, so change post_per_page to 1
+        $args = array(
                 'type' => 'post',
                 'posts_per_page' => 1,
-                'category__in' => $category->term_id, // get term_id 
+                // category has to be inside the loop we are calling...
+                // we go inside $category and look for term_id (see var dump - everything was in there)
+                'category__in' => $category->term_id, // so in this loop we are creating 3 queries...
+                // every time we loop, we call one post, then loop again to call the second post... third...
                 'category__not_in' => array(1,10),
             );
-            
             $lastBlog = new WP_Query( $args );
             
             if( $lastBlog->have_posts() ):
             
                 while($lastBlog->have_posts() ) : $lastBlog->the_post(); echo 'THIS IS page-home.php THE FORMAT: '.get_post_format(); ?>
+                <!-- took everything out of here and put it in content.php -->
+                <!-- below is mark up for featured -->
                 <div class="col-xs-12 col-sm-4">
                 <?php get_template_part('content','featured'); ?>
                 </div>
@@ -30,10 +44,33 @@
                 <?php endwhile;
             endif;
             wp_reset_postdata();
+            // this is the end...
         endforeach;
         
+        
+            $args = array(
+                'type' => 'post',
+                'posts_per_page' => 3,
+                'category__in' => array(7,8,9),
+                'category__not_in' => array(1,10),
+            );
+            $lastBlog = new WP_Query( $args );
+            
+            if( $lastBlog->have_posts() ):
+            
+                while($lastBlog->have_posts() ) : $lastBlog->the_post(); echo 'THIS IS page-home.php THE FORMAT: '.get_post_format(); ?>
+                <!-- took everything out of here and put it in content.php -->
+                <!-- below is mark up for featured -->
+                <div class="col-xs-12 col-sm-4">
+                <?php get_template_part('content','featured'); ?>
+                </div>
+                
+                <?php endwhile;
+            endif;
+            wp_reset_postdata();
         ?>
 </div>
+    
     
     
 <div class="row">   
